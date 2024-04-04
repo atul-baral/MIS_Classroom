2ab6992aad53b3db2930cd654645f53a30f11725
sthakeshar <shresthakeshar291@gmail.com>
2024-04-04 10:59:55 +0545

    Areas added

---
+++ MIS_Classroom/Areas/Teacher/Controllers/TeacherController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MIS_Classroom.Areas.Teacher.Controllers
+{
+    public class TeacherController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
