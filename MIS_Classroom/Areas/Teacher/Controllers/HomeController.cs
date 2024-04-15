472499390a33469d1790a9aafc372aa88903a6e4
saurav shakya <69755787+1saura2@users.noreply.github.com>
2024-04-15 16:53:29 +0545

    Question UI added further update required

--- MIS_Classroom/Areas/Teacher/Controllers/HomeController.cs
+++ MIS_Classroom/Areas/Teacher/Controllers/HomeController.cs
@@ -1,4 +1,4 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿/*using Microsoft.AspNetCore.Mvc;
 using MIS_Classroom.Areas.Teacher.Models;
 using MIS_Classroom.Models;
 using System.Linq;
@@ -41,3 +41,76 @@ namespace MIS_Classroom.Areas.Teacher.Controllers
         }
     }
 }
+*/
+using Microsoft.AspNetCore.Mvc;
+using MIS_Classroom.Areas.Teacher.Models;
+using MIS_Classroom.Models;
+using System.Linq;
+
+namespace MIS_Classroom.Areas.Teacher.Controllers
+{
+    [Area("Teacher")]
+    public class HomeController : Controller
+    {
+        private readonly tattsContext _context;
+
+        public HomeController(tattsContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult Index(int subjectId)
+        {
+            var viewModel = new AddQuestionViewModel
+            {
+                Subjects = _context.TechengineeMisSubjects.ToList(),
+                SubjectId = subjectId,
+                Questions = _context.TechengineeMisQuestions
+                    .Where(q => q.SubjectCode == subjectId)
+                    .ToList()
+            };
+            return View(viewModel);
+        }
+
+        [HttpPost]
+        public IActionResult Index(int subjectId, string questionText)
+        {
+            if (!string.IsNullOrEmpty(questionText))
+            {
+                var question = new TechengineeMisQuestion
+                {
+                    SubjectCode = subjectId,
+                    QuestionsTxt = questionText
+                };
+                _context.TechengineeMisQuestions.Add(question);
+                _context.SaveChanges();
+            }
+            return RedirectToAction(nameof(Index), new { subjectId });
+        }
+
+        [HttpPost]
+        public IActionResult Update(int id, string updatedQuestionText, int subjectId)
+        {
+            var question = _context.TechengineeMisQuestions.Find(id);
+            if (question != null)
+            {
+                question.QuestionsTxt = updatedQuestionText;
+                _context.SaveChanges();
+            }
+            return RedirectToAction(nameof(Index), new { subjectId });
+        }
+
+        [HttpPost]
+        public IActionResult Delete(int id, int subjectId)
+        {
+            var question = _context.TechengineeMisQuestions.Find(id);
+            if (question != null)
+            {
+                _context.TechengineeMisQuestions.Remove(question);
+                _context.SaveChanges();
+            }
+            return RedirectToAction(nameof(Index), new { subjectId });
+        }
+    }
+}
+
--- MIS_Classroom/Areas/Teacher/Models/AddQuestionViewModel.cs
+++ MIS_Classroom/Areas/Teacher/Models/AddQuestionViewModel.cs
@@ -1,9 +1,23 @@
-﻿using MIS_Classroom.Models;
+﻿/*using MIS_Classroom.Models;
 
 namespace MIS_Classroom.Areas.Teacher.Models
 {
     public class AddQuestionViewModel
     {
         public List<TechengineeMisSubject> Subjects { get; set; }
+    }
+}
+*/
+
+using MIS_Classroom.Models;
+using System.Collections.Generic;
+
+namespace MIS_Classroom.Areas.Teacher.Models
+{
+    public class AddQuestionViewModel
+    {
+        public List<TechengineeMisSubject> Subjects { get; set; }
+        public List<TechengineeMisQuestion> Questions { get; set; }
+        public int SubjectId { get; set; }
     }
 }
